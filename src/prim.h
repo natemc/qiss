e2b02fc785bdc7a82e817d0fa2ae33d9dbc19114
Nate McNamara <nate@mcnamara.net>
2022-04-20 17:26:01 -0400

    Add integral type promotion for | and &

--- src/prim.h
+++ src/prim.h
@@ -11,7 +11,7 @@
 template <class X> struct is_prim: std::false_type {};
 template <class X> inline constexpr bool is_prim_v = is_prim<X>::value;
 
-#define PRIM(TYPE, REP, EQTEST, LTTEST, NULLTEST) struct TYPE {       \
+#define PRIM_BEGIN(TYPE, REP, EQTEST, LTTEST, NULLTEST) struct TYPE { \
   using rep = REP;                                                    \
   TYPE() = default;                                                   \
   explicit constexpr TYPE(rep x_): x(x_) {}                           \
@@ -19,7 +19,9 @@ template <class X> inline constexpr bool is_prim_v = is_prim<X>::value;
   explicit constexpr operator bool      () const { return bool(x); }  \
   bool operator< (TYPE y) const { return LTTEST; }                    \
   bool operator==(TYPE y) const { return EQTEST; }                    \
-  bool is_null() const { return NULLTEST; }                           \
+  bool is_null() const { return NULLTEST; }
+
+#define PRIM_END(TYPE)                                                \
 private:                                                              \
   rep x;                                                              \
 };                                                                    \
@@ -28,6 +30,9 @@ inline bool operator> (TYPE x, TYPE y) { return y < x; }              \
 inline bool operator<=(TYPE x, TYPE y) { return !(y < x); }           \
 inline bool operator>=(TYPE x, TYPE y) { return !(x < y); }           \
 inline bool operator!=(TYPE x, TYPE y) { return !(x == y); }
+
+#define PRIM(TYPE, REP, EQTEST, LTTEST, NULLTEST) \
+PRIM_BEGIN(TYPE, REP, EQTEST, LTTEST, NULLTEST) PRIM_END(TYPE)
 
 #define PRIM_EQ_LT(TYPE, REP, NULLTEST) \
 PRIM(TYPE, REP, x == y.x, x < y.x, NULLTEST)
@@ -45,7 +50,30 @@ constexpr D ND = D(std::numeric_limits<D::rep>::min());
 constexpr D WD = D(std::numeric_limits<D::rep>::max());
 D ymd2date(int y, int m, int d); // DOES NO CHECKING!
 
-PRIM_EQ_LT(J, int64_t, x == std::numeric_limits<rep>::min())
+PRIM_BEGIN(I, int32_t, x == y.x, x < y.x, x == std::numeric_limits<rep>::min())
+    explicit I(B o): x(B::rep(o)) {}
+    explicit I(X o): x(X::rep(o)) {}
+PRIM_END(I)
+constexpr I NI = I(std::numeric_limits<I::rep>::min());
+constexpr I WI = I(std::numeric_limits<I::rep>::max());
+inline I& operator+=(I& x, B y) { x = I(I::rep(x) + B::rep(y)); return x; }
+inline I& operator+=(I& x, I y) { x = I(I::rep(x) + I::rep(y)); return x; }
+inline I& operator-=(I& x, I y) { x = I(I::rep(x) - I::rep(y)); return x; }
+inline I& operator-=(I& x, B y) { return x -= I(I::rep(B::rep(y))); }
+inline I& operator++(I& x) { x = I(I::rep(x) + 1); return x; }
+inline I operator++(I& x, int) { I old(x); ++x; return old; }
+inline I operator-(I x) { return I(-I::rep(x)); }
+inline I operator*(I x, I y) { return I(I::rep(x) * I::rep(y)); }
+inline I operator-(I x, I y) { return I(I::rep(x) - I::rep(y)); }
+inline I operator+(I x, I y) { return I(I::rep(x) + I::rep(y)); }
+inline I operator/(I x, I y) { return I::rep(y)? I(I::rep(x) / I::rep(y)) : NI; }
+inline I operator%(I x, I y) { return I::rep(y)? I(I::rep(x) % I::rep(y)) : NI; }
+
+PRIM_BEGIN(J, int64_t, x == y.x, x < y.x, x == std::numeric_limits<rep>::min())
+    explicit J(B o): x(B::rep(o)) {}
+    explicit J(I o): x(I::rep(o)) {}
+    explicit J(X o): x(X::rep(o)) {}
+PRIM_END(J)
 constexpr J NJ = J(std::numeric_limits<J::rep>::min());
 constexpr J WJ = J(std::numeric_limits<J::rep>::max());
 inline J& operator+=(J& x, J y) { x = J(J::rep(x) + J::rep(y)); return x; }
@@ -63,24 +91,13 @@ inline J operator+(J x, J y) { return J(J::rep(x) + J::rep(y)); }
 inline J operator/(J x, J y) { return J::rep(y)? J(J::rep(x) / J::rep(y)) : NJ; }
 inline J operator%(J x, J y) { return J::rep(y)? J(J::rep(x) % J::rep(y)) : NJ; }
 
-PRIM_EQ_LT(I, int32_t, x == std::numeric_limits<rep>::min())
-constexpr I NI = I(std::numeric_limits<I::rep>::min());
-constexpr I WI = I(std::numeric_limits<I::rep>::max());
-inline I& operator+=(I& x, B y) { x = I(I::rep(x) + B::rep(y)); return x; }
-inline I& operator+=(I& x, I y) { x = I(I::rep(x) + I::rep(y)); return x; }
-inline I& operator-=(I& x, I y) { x = I(I::rep(x) - I::rep(y)); return x; }
-inline I& operator-=(I& x, B y) { return x -= I(I::rep(B::rep(y))); }
-inline I& operator++(I& x) { x = I(I::rep(x) + 1); return x; }
-inline I operator++(I& x, int) { I old(x); ++x; return old; }
-inline I operator-(I x) { return I(-I::rep(x)); }
-inline I operator*(I x, I y) { return I(I::rep(x) * I::rep(y)); }
-inline I operator-(I x, I y) { return I(I::rep(x) - I::rep(y)); }
-inline I operator+(I x, I y) { return I(I::rep(x) + I::rep(y)); }
-inline I operator/(I x, I y) { return I::rep(y)? I(I::rep(x) / I::rep(y)) : NI; }
-inline I operator%(I x, I y) { return I::rep(y)? I(I::rep(x) % I::rep(y)) : NI; }
-
 bool fuzzy_match(double x, double y);
-PRIM(F, double, fuzzy_match(x, y.x), x < y.x, std::isnan(x))
+PRIM_BEGIN(F, double, fuzzy_match(x, y.x), x < y.x, std::isnan(x))
+    explicit F(B o): x(B::rep(o)) {}
+    explicit F(I o): x(I::rep(o)) {}
+    explicit F(J o): x(J::rep(o)) {}
+    explicit F(X o): x(X::rep(o)) {}
+PRIM_END(F)
 constexpr F NF = F(std::numeric_limits<F::rep>::quiet_NaN());
 constexpr F WF = F(std::numeric_limits<F::rep>::infinity());
 inline F operator-(F x) { return F(-F::rep(x)); }
@@ -194,6 +211,10 @@ inline X operator-(X x, int y) { return X(X::rep(X::rep(x) - y)); }
 inline X operator+(X x, X   y) { return X(X::rep(x) + X::rep(y)); }
 inline X operator-(X x, X   y) { return X(X::rep(x) - X::rep(y)); }
 
+inline B operator==(B x, I y) { return B(B::rep(x) == I::rep(y)); }
+inline B operator==(B x, J y) { return B(B::rep(x) == J::rep(y)); }
+inline B operator==(I x, B y) { return B(I::rep(x) == B::rep(y)); }
+inline B operator==(J x, B y) { return B(J::rep(x) == B::rep(y)); }
 inline B operator==(F x, I y) { return B(F::rep(x) == I::rep(y)); }
 inline B operator==(I x, F y) { return B(I::rep(x) == F::rep(y)); }
 inline B operator==(F x, J y) { return B(F::rep(x) == F::rep(J::rep(y))); }
@@ -201,6 +222,10 @@ inline B operator==(J x, F y) { return B(F::rep(J::rep(x)) == F::rep(y)); }
 inline B operator==(I x, J y) { return B(I::rep(x) == J::rep(y)); }
 inline B operator==(J x, I y) { return B(J::rep(x) == I::rep(y)); }
 
+inline B operator<(B x, I y) { return B(B::rep(x) < I::rep(y)); }
+inline B operator<(B x, J y) { return B(B::rep(x) < J::rep(y)); }
+inline B operator<(I x, B y) { return B(I::rep(x) < B::rep(y)); }
+inline B operator<(J x, B y) { return B(J::rep(x) < B::rep(y)); }
 inline B operator<(F x, I y) { return B(F::rep(x) < I::rep(y)); }
 inline B operator<(I x, F y) { return B(I::rep(x) < F::rep(y)); }
 inline B operator<(F x, J y) { return B(F::rep(x) < F::rep(J::rep(y))); }
@@ -208,6 +233,10 @@ inline B operator<(J x, F y) { return B(F::rep(J::rep(x)) < F::rep(y)); }
 inline B operator<(I x, J y) { return B(I::rep(x) < J::rep(y)); }
 inline B operator<(J x, I y) { return B(J::rep(x) < I::rep(y)); }
 
+inline B operator>(B x, I y) { return B(B::rep(x) > I::rep(y)); }
+inline B operator>(B x, J y) { return B(B::rep(x) > J::rep(y)); }
+inline B operator>(I x, B y) { return B(I::rep(x) > B::rep(y)); }
+inline B operator>(J x, B y) { return B(J::rep(x) > B::rep(y)); }
 inline B operator>(F x, I y) { return B(F::rep(x) > I::rep(y)); }
 inline B operator>(I x, F y) { return B(I::rep(x) > F::rep(y)); }
 inline B operator>(F x, J y) { return B(F::rep(x) > F::rep(J::rep(y))); }
--- src/relop.cpp
+++ src/relop.cpp
@@ -1,8 +1,23 @@
 #include <relop.h>
 #include <atomic.h>
+#include <type_traits>
+
+namespace std {
+    template <> struct common_type<B, I> { using type = I; };
+    template <> struct common_type<I, B> { using type = I; };
+    template <> struct common_type<B, J> { using type = J; };
+    template <> struct common_type<J, B> { using type = J; };
+    template <> struct common_type<F, I> { using type = F; };
+    template <> struct common_type<I, F> { using type = F; };
+    template <> struct common_type<F, J> { using type = F; };
+    template <> struct common_type<J, F> { using type = F; };
+    template <> struct common_type<I, J> { using type = J; };
+    template <> struct common_type<J, I> { using type = J; };
+}
 
 ATOMIC_BEGIN(Eq, =, B(x == y))
-    ATOMIC1(B); ATOMIC1(C); ATOMIC1(D);
+    ATOMIC1(B); ATOMIC_BOTH_WAYS(B,I); ATOMIC_BOTH_WAYS(B,J);
+    ATOMIC1(C); ATOMIC1(D);
     ATOMIC1(F); ATOMIC_BOTH_WAYS(F,I); ATOMIC_BOTH_WAYS(F,J);
     ATOMIC1(H);
     ATOMIC1(I); ATOMIC_BOTH_WAYS(I,J);
@@ -11,7 +26,8 @@ ATOMIC_BEGIN(Eq, =, B(x == y))
 ATOMIC_END(eq)
 
 ATOMIC_BEGIN(Less, <, B(x < y))
-    ATOMIC1(B); ATOMIC1(C); ATOMIC1(D);
+    ATOMIC1(B); ATOMIC_BOTH_WAYS(B,I); ATOMIC_BOTH_WAYS(B,J);
+    ATOMIC1(C); ATOMIC1(D);
     ATOMIC1(F); ATOMIC_BOTH_WAYS(F,I); ATOMIC_BOTH_WAYS(F,J);
     ATOMIC1(H);
     ATOMIC1(I); ATOMIC_BOTH_WAYS(I,J);
@@ -20,7 +36,8 @@ ATOMIC_BEGIN(Less, <, B(x < y))
 ATOMIC_END(less)
 
 ATOMIC_BEGIN(Greater, >, B(x > y))
-    ATOMIC1(B); ATOMIC1(C); ATOMIC1(D);
+    ATOMIC1(B); ATOMIC_BOTH_WAYS(B,I); ATOMIC_BOTH_WAYS(B,J);
+    ATOMIC1(C); ATOMIC1(D);
     ATOMIC1(F); ATOMIC_BOTH_WAYS(F,I); ATOMIC_BOTH_WAYS(F,J);
     ATOMIC1(H);
     ATOMIC1(I); ATOMIC_BOTH_WAYS(I,J);
@@ -28,14 +45,24 @@ ATOMIC_BEGIN(Greater, >, B(x > y))
     ATOMICOO();
 ATOMIC_END(greater)
 
-ATOMIC_BEGIN(Max, |, x<y?y:x) // cppcheck-suppress syntaxError
-    ATOMIC1(B); ATOMIC1(C); ATOMIC1(D); ATOMIC1(F); ATOMIC1(H); ATOMIC1(I);
+ATOMIC_BEGIN(Max, |, (x < y? std::common_type_t<decltype(x), decltype(y)>(y)
+                           : std::common_type_t<decltype(x), decltype(y)>(x))) // cppcheck-suppress syntaxError
+    ATOMIC1(B); ATOMIC_BOTH_WAYS(B,I); ATOMIC_BOTH_WAYS(B,J);
+    ATOMIC1(C); ATOMIC1(D);
+    ATOMIC1(F); ATOMIC_BOTH_WAYS(F,I); ATOMIC_BOTH_WAYS(F,J);
+    ATOMIC1(H);
+    ATOMIC1(I); ATOMIC_BOTH_WAYS(I,J);
     ATOMIC1(J); ATOMIC1(S); ATOMIC1(T); ATOMIC1(X);
     ATOMICOO();
 ATOMIC_END(max)
 
-ATOMIC_BEGIN(Min, &, x<y?x:y)
-    ATOMIC1(B); ATOMIC1(C); ATOMIC1(D); ATOMIC1(F); ATOMIC1(H); ATOMIC1(I);
+ATOMIC_BEGIN(Min, |, (x < y? std::common_type_t<decltype(x), decltype(y)>(x)
+                           : std::common_type_t<decltype(x), decltype(y)>(y))) // cppcheck-suppress syntaxError
+    ATOMIC1(B); ATOMIC_BOTH_WAYS(B,I); ATOMIC_BOTH_WAYS(B,J);
+    ATOMIC1(C); ATOMIC1(D);
+    ATOMIC1(F); ATOMIC_BOTH_WAYS(F,I); ATOMIC_BOTH_WAYS(F,J);
+    ATOMIC1(H);
+    ATOMIC1(I); ATOMIC_BOTH_WAYS(I,J);
     ATOMIC1(J); ATOMIC1(S); ATOMIC1(T); ATOMIC1(X);
     ATOMICOO();
 ATOMIC_END(min)
